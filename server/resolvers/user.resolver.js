23099a26160b2816f725d61f99f1c61c9971c0cb
Viktor <vitya.getman.79@gmail.com>
2024-05-03 12:50:21 +0200

    passport + middlewares configuration

--- server/resolvers/user.resolver.js
+++ server/resolvers/user.resolver.js
@@ -2,10 +2,10 @@ import { dummyUsers } from "../data/index.js";
 
 const userResolver = {
   Query: {
-    users: () => {
+    users: (_, _, { req, res }) => {
       return dummyUsers;
     },
-    user: (_, { userId }) => {
+    user: (_, { userId }, { req, res }) => {
       return dummyUsers.find((user) => user._id === userId);
     },
   },
--- server/server.js
+++ server/server.js
@@ -1,15 +1,50 @@
 import http from "node:http";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import passport from "passport";
+import session from "express-session";
+import ConnectMongo from "connect-mongodb-session";
+import { buildContext } from "graphql-passport";
 
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
+import { connectDB } from "./db/connectDB.js";
+import { configurePassport } from "./passport/passport.config.js";
+
+dotenv.config();
+configurePassport();
 
 const app = express();
 const httpServer = http.createServer(app);
+
+const MongoDBStore = ConnectMongo(session);
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+store.on("error", (err) => console.log(err));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    },
+    store: store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -21,13 +56,17 @@ await server.start();
 
 app.use(
   "/",
-  cors(),
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
   express.json(),
   expressMiddleware(server, {
-    context: async ({ req }) => ({ req }),
+    context: async ({ req, res }) => buildContext({ req, res }),
   })
 );
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
 
 console.log(`Server ready at port http://localhost:4000/`);
